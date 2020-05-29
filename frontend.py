f39acd2a657b1dba30a14a4084933c1765ee3974
James Savory <james.pa.savory@gmail.com>
2020-05-29 10:12:15 -0400

    all form data being captured

--- frontend.py
+++ frontend.py
@@ -8,49 +8,46 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+print(app.config['UPLOAD_FOLDER'])
 
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/', methods=["GET","POST"])
 def home():
     if request.method == 'POST':
-        if 'wine-label' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            wine_label = request.files['wine-label']
+            review_site = request.files['review-site']
 
-        wine_label = request.files["wine-label"]
+            if wine_label and review_site:
+                wine_label_filename = secure_filename(wine_label.filename)
+                wine_label.save(os.path.join(app.config['UPLOAD_FOLDER'], wine_label_filename))
 
+                review_site_filename = secure_filename(review_site.filename)
+                review_site.save(os.path.join(app.config['UPLOAD_FOLDER'], review_site_filename))
 
-        if wine_label and allowed_file(wine_label.filename):
-            wine_label_filename = secure_filename(wine_label.filename)
-            # print(type(wine_label_filename))
+            return redirect(url_for('upload_file', review_site=review_site_filename, wine_label=wine_label_filename))
 
-            wine_label.save(os.path.join(app.config['UPLOAD_FOLDER'], wine_label_filename))
-
-        # review_site = request.files["review-site"]
-        # review_site_secure = secure_filename(review_site.filename)
-        # review_site_secure.save(os.path.join(app.config['UPLOAD_FOLDER'], review_site_secure))
-
-        description = request.form['description']
-        points = request.form['points']
-
-        return redirect(url_for('upload_file',
-                                wine_label=wine_label_filename
-                                ))
-
+    else:
+        flash('No file part')
 
         # return redirect(url_for('view_sign', description=form_description, points=points,
         #                         wine_label=wine_label, review_site=review_site))
 
     return render_template('home.html')
 
-@app.route('/uploads/<wine_label>')
-def upload_file(wine_label):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               wine_label)
+@app.route('/uploads/<wine_label>/<review_site>')
+def upload_file(wine_label, review_site):
+    # wine_label = send_from_directory(app.config['UPLOAD_FOLDER'], wine_label)
+    # review_site = send_from_directory(app.config['UPLOAD_FOLDER'], review_site)
+
+    wine_label_filename = wine_label
+    review_site_filename = review_site
+
+    return render_template('sign.html', wine_label=wine_label_filename, review_site_file=review_site_filename)
 
 
 
@@ -62,5 +59,5 @@ def view_sign(description, points,wine_label,review_site):
     review_site_var = review_site
 
 
-    return render_template('sign.html', description=description_var, wine_label=wine_label_var,
+    return render_template('ticket.html', description=description_var, wine_label=wine_label_var,
                            review_site=review_site_var, points=points_var)
---
+++ notes.md
@@ -0,0 +1,17 @@
+  Form:
+- Form with relevant fields [x]
+- Capture the data [x]
+
+
+Label:
+- Get https://github.com/Kozea/WeasyPrint/tree/gh-pages/samples/ticket working locally. [-]
+- Adjust the template a little-- learn it responds (css/html) [-]
+- build a static example of the label. (1 on a page) 
+- Add images? Add a star behind the rating?
+
+
+Combine:
+- Flask-Weasy, provide data from form into template and render,
+  or create a 'static' html file to use for rendering.
+ 
+ 
